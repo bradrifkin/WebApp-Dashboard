2f2112bd0f2a66190f5687fdc6fbfee7454c555d
Brad Rifkin <brifkin17@gmail.com>
2018-11-06 14:28:09 -0500

    Add Sass files

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// const selected = document.querySelector('.selected');
+
+// selected.parentNode.parentNode.style.borderBottom = '3px solid #81C98F';
+// selected.parentNode.parentNode.style.borderRadius = '3px';
